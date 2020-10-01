60093292ca30f5f121107c5271ae7d2702dc66e4
Chakradhar <chakradhar.gorthi@gmail.com>
2020-10-02 00:25:18 +0530

    Day one of JS, learnt how to declare variables in JS and the structure of object and some array methods

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+let bava = 5;
+console.log(bava);
+// alert(bava);
+// alert("Hello this is a sample alert box");
+while (bava != undefined) {
+  console.log("something is going on bro");
+  bava = undefined;
+}
+let animals = ["bunny", "tiger", "chicken", "horse", "lion"];
+console.log(animals[0], animals[2], animals[4]);
+console.log(animals.length);
+
+let student = {
+	name:'bunny',
+	branch:'cse',
+	StudId:1
+};
