e6362130239133d75927932b159b8632365a3cb7
Chakradhar <chakradhar.gorthi@gmail.com>
2020-10-06 00:15:12 +0530

    a basic version of todolist app which simply adds the task you enter in text field

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 "use strict";
-let student = {
-	Name: 'Bunny',
-	Year: 4,
-	Branch: 'CSE',
-	Section: 'A',
-	Subjects: ['data structures', 'Java', 'M1', 'Physics', 'Machine learning'],
-	Enter: function(){
-		console.log('inside a method which lets you enter');
-	},
-	Exit: function(){
-		console.log('inside a method which lets you exit');
-		console.log("hey");
-		console.log(`Branch of ${this.Name} is ${this.Branch}`);
-		this.Subjects.forEach(subject => {
-			console.log(`my subjects are ${subject}`);
-		});
-	}
-};
-student.Exit();
+function clicked() {
+  console.log("clicked");
+  let content = document.querySelector("input").value;
+  console.log(content);
+  let list = document.querySelector("ul");
+  console.log(list);
+  let tag = document.createElement("li");
+  let text = document.createTextNode(content);
+  tag.appendChild(text);
+  list.appendChild(tag);
+}
