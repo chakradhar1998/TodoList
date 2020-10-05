34d113cfbe09e5b22027be99907cd9ff87f93ed8
Chakradhar <chakradhar.gorthi@gmail.com>
2020-10-05 21:55:26 +0530

    learned about object and various ways in which it is usefull.

--- script.js
+++ script.js
@@ -1,52 +1,20 @@
 "use strict";
-let bava = 5;
-console.log(bava);
-// alert(bava);
-// alert("Hello this is a sample alert box");
-while (bava != undefined) {
-  console.log("something is going on bro");
-  bava = undefined;
-}
-let animals = ["bunny", "tiger", "chicken", "horse", "lion"];
-console.log(animals[0], animals[2], animals[4]);
-console.log(animals.length);
-
 let student = {
-  name: "bunny",
-  branch: "cse",
-  StudId: 1,
-};
-
-console.log(student);
-
-let firstName = 'bunny';
-let lastName = 'gorthi';
-let fullName = firstName + ' ' + lastName;
-console.log(fullName, fullName.length);
-console.log(fullName.toUpperCase());
-
-let Email = 'chakradhar.gorthi@gmail.com';
-console.log(Email);
-
-const speak = function(name){
-	console.log(`${name}`);
-};
-
-speak('bunny');
-
-const calc = () => console.log(5);
-
-calc();
-
-const greet = () => console.log('hello, world');
-greet();
-
-const bill = (products, tax) => {
-	let total = 0;
-	for(let i = 0; i < products.length; i++){
-		total += products[i] + products[i] * tax;
+	Name: 'Bunny',
+	Year: 4,
+	Branch: 'CSE',
+	Section: 'A',
+	Subjects: ['data structures', 'Java', 'M1', 'Physics', 'Machine learning'],
+	Enter: function(){
+		console.log('inside a method which lets you enter');
+	},
+	Exit: function(){
+		console.log('inside a method which lets you exit');
+		console.log("hey");
+		console.log(`Branch of ${this.Name} is ${this.Branch}`);
+		this.Subjects.forEach(subject => {
+			console.log(`my subjects are ${subject}`);
+		});
 	}
-	return total;
 };
-
-console.log(bill([10,15,20,30], 0.2));
+student.Exit();
