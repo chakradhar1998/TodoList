21b90e120c4461bbdf1e5049faeb0f6813889ac3
Chakradhar <chakradhar.gorthi@gmail.com>
2020-10-06 14:00:56 +0530

    added dark background and centered both text and buttons.

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ function clicked() {
   console.log("clicked");
   let content = document.querySelector("input").value;
   console.log(content);
-  let list = document.querySelector("ul");
-  console.log(list);
+  //let list = document.querySelector("ul");
+  let list = document.getElementsByClassName("list-items");
+  console.log(list[0]);
   let tag = document.createElement("li");
   let text = document.createTextNode(content);
   tag.appendChild(text);
-  list.appendChild(tag);
+  list[0].appendChild(tag);
 }
